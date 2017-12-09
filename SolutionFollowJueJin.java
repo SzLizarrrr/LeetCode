f496753b5aa8d2a613b0d47a6054a5ff36c1eb3e
szlizarrrr <yf8189@gmail.com>
2017-12-09 03:03:53 -0800

    format beautiful

--- SolutionFollowJueJin.java
+++ SolutionFollowJueJin.java
@@ -81,23 +81,23 @@ public class SolutionFollowJueJin {
                     result += (result >= 5 ? -1 : 1);
                     break;
                 case 'V':
-		        	result += 5;
-		        	break;
-		        case 'X':
-		        	result += 10 * (result >= 50 ? -1 : 1);
-		        	break;
-		        case 'L':
-		        	result += 50;
-		        	break;
-		        case 'C':
-		        	result += 100 * (result >= 500 ? -1 : 1);
-		        	break;
-		        case 'D':
-		        	result += 500;
-		        	break;
-		        case 'M':
-		        	result += 1000;
-		        	break;
+                    result += 5;
+                    break;
+                case 'X':
+                    result += 10 * (result >= 50 ? -1 : 1);
+                    break;
+                case 'L':
+                    result += 50;
+                    break;
+                case 'C':
+                    result += 100 * (result >= 500 ? -1 : 1);
+                    break;
+                case 'D':
+                    result += 500;
+                    break;
+                case 'M':
+                    result += 1000;
+                    break;
             }
         }
         return result;
